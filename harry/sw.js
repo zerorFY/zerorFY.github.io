33b5f91c7e4a58186d33b21faf69737f8a6b5f47
zerorFY <fuyu6430@gmail.com>
2026-09-02 11:49:16 -0400

    Publish Harry interactive storybook

---
+++ harry/sw.js
@@ -0,0 +1,5 @@
+const CACHE='magical-seven-books-v2';
+const SHELL=['/harry/','/harry/index.html','/harry/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/harry/index.html'))))});
